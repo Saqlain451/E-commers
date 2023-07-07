6ed6eb0c8d79d2ed29221873af3510552d23494c
Saqlain451 <jobs.sakil.mustak@gmail.com>
2023-07-07 22:49:17 +0530

    scroll top added

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import Home from "./pages/Home.jsx";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Dashboard from "./pages/Dashboard.jsx";
+import ScrollToTop from "./components/ScrollRestore.jsx";
 
 const App = () => {
     return (<>
         <BrowserRouter>
+            <ScrollToTop/>
             <Routes>
                 <Route index element={<Home/>}/>
                 <Route path={"/dashboard"} element={<Dashboard/>}/>
